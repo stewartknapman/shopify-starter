e2080a73827abd6c873ef2ec89391b0871737430
Stewart Knapman <info@stewartknapman.com>
2016-07-13 16:52:40 +1200

    remove pollyfill from mq-size

--- _src/js/_mq-size.js
+++ _src/js/_mq-size.js
@@ -1,5 +1,3 @@
-require('./_pollyfills/pollyfill.getComputedStyle.js')();
-
 module.exports.size = function () {
   return window.getComputedStyle(document.body,':after').getPropertyValue('content').replace(/['"]/g, '');
 };
